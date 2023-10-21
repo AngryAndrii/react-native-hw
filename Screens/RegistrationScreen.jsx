071ed78b774a84b37fcb002148e23588142d2294
Andrii Chychkan <mywork1728@gmail.com>
2023-10-21 23:42:42 +0300

    started form

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,6 +1,10 @@
-import { ImageBackground, TextInput } from "react-native";
-// import BackgroundImage from "../images/photo-bg.png";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 const wallpaper = require("../images/photo-bg.png");
 
@@ -12,7 +16,15 @@ export default function RegistrationScreen() {
     //   <TextInput placeholder="Type text" style={styles.input} /> */}
     // </View>
     <View style={styles.container}>
-      <ImageBackground source={wallpaper} style={styles.backgroundImage} />
+      <ImageBackground source={wallpaper} style={styles.backgroundImage}>
+        <View style={styles.regContainer}>
+          <View style={styles.avatarWrapper}></View>
+          <Text style={styles.regText}>Реєстрація</Text>
+          <TextInput style={styles.textInput} />
+          <TextInput style={styles.textInput} />
+          <TextInput style={styles.textInput} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -30,5 +42,37 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     width: "100%",
     height: "100%",
+  },
+  regContainer: {
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingTop: 92,
+    paddingHorizontal: 16,
+    height: 550,
+    backgroundColor: "#fff",
+    position: "relative",
+  },
+  avatarWrapper: {
+    position: "absolute",
+    width: 120,
+    height: 120,
+    backgroundColor: "#f6f6f6",
+    top: -60,
+    left: 128,
+    borderRadius: 16,
+  },
+  regText: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#212121",
+    marginBottom: 32,
+  },
+  textInput: {
+    height: 50,
+    backgroundColor: "#f6f6f6",
+    borderWidth: 1,
+    borderColor: "#e8e8e8",
+    borderRadius: 8,
+    marginBottom: 16,
   },
 });
